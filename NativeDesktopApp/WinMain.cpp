6a6bea5e3b9d9e1a5fefea495d943ceb43a4b06b
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-04-22 13:48:58 -0700

    update managed app RIDs, update cpp app

--- NativeDesktopApp/WinMain.cpp
+++ NativeDesktopApp/WinMain.cpp
@@ -7,14 +7,21 @@ using namespace Windows::Foundation;
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
     init_apartment(apartment_type::single_threaded);
-    Uri uri(L"http://aka.ms/cppwinrt");
-    ::MessageBoxW(::GetDesktopWindow(), uri.AbsoluteUri().c_str(), L"C++/WinRT Desktop Application", MB_OK);
-    Coord a();
-    // auto aStr = a.ToString();
-    double x = 39.0;
-    double y = 80.0;
-    // auto aToB = a.Distance(a);
-    // Coord b{x,y};
-    // auto aStr = a.ToString();
-    // auto bStr = b.ToString();
+    std::wostringstream wostringstream;
+
+    Coord a(0.0, 0.0);
+    auto aStr = a.ToString();
+    double bx = 39.0;
+    double by = 80.0;
+    Coord b(bx, by);
+    auto bStr = b.ToString();
+    auto aToB = a.Distance(b);
+
+    wostringstream << L"We expect the distance between " << aStr.c_str() << L" and " << bStr.c_str() << L" to be 89.0" << std::endl;
+
+    std::wostringstream anotherStream;
+    anotherStream << L"Calculated distance was " << a.Distance(b) << std::endl;
+
+    ::MessageBoxW(::GetDesktopWindow(), wostringstream.str().c_str(), L"C++/WinRT Desktop Application", MB_OK);
+    ::MessageBoxW(::GetDesktopWindow(), anotherStream.str().c_str(), L"Another pop up", MB_OK);
 }
